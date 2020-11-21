027e943050f3e4e5e7c19bd8d5cbc3abb418d061
geoffrey112 <geoffrey.legall@orange.fr>
2020-11-21 15:36:49 +0100

    update without param ex1 (finished)

--- Functions/src/js/func.js
+++ Functions/src/js/func.js
@@ -28,7 +28,46 @@ function myDate(){
   }
 
   const date = new Date().getDate();
-  const month = new Date().getMonth() + 1;
+  let month = new Date().getMonth();
+  switch(month){
+    case 0:
+      month = "January";
+      break;
+    case 1:
+      month = "February";
+    break;
+    case 2 :
+      month = "March";
+    break;
+    case 3:
+      month = "April";
+    break;
+    case 4:
+      month = "May";
+    break;
+    case 5:
+      month = "June";
+    break;
+    case 6:
+      month = "July";
+    break;
+    case 7:
+      month = "August";
+    break;
+    case 8:
+      month = "September";
+    break;
+    case 9:
+      month = "October";
+    break;
+    case 10:
+      month = "November";
+    break;
+    case 11:
+      month = "December";
+    break;
+  }
+
   const years = new Date().getFullYear();
   const hours = new Date().getHours();
   const minutes = new Date().getMinutes();
@@ -36,7 +75,6 @@ function myDate(){
 
   return `${day} ${date} ${month} ${years}, ${hours}:${minutes} ${amPm}`;
 }
-
 
 document.getElementById("date").innerHTML = myDate();
 
